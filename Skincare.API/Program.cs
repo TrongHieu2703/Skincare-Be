7cb17e5b0d9173fff289b36fff7bbaa62695c0cd
TrongHieu2709 <tronghieu848@gmail.com>
2025-02-15 14:08:06 +0700

    update

--- Skincare.API/Program.cs
+++ Skincare.API/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using Skincare.Repositories.Context;
+
 namespace Skincare.API
 {
     public class Program
@@ -8,6 +11,9 @@ namespace Skincare.API
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            //Add dbcontext
+            builder.Services.AddDbContext<SWP391Context>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
