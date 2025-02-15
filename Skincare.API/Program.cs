1ab37ec5664c45285da79e1c7455f47e899d9dec
TrongHieu2709 <tronghieu848@gmail.com>
2025-02-15 14:17:21 +0700

    update

--- Skincare.API/Program.cs
+++ Skincare.API/Program.cs
@@ -1,4 +1,4 @@
-
+﻿
 using Microsoft.EntityFrameworkCore;
 using Skincare.Repositories.Context;
 
@@ -13,12 +13,26 @@ namespace Skincare.API
             // Add services to the container.
             //Add dbcontext
             builder.Services.AddDbContext<SWP391Context>(options =>
-            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+             options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            // Thêm chính sách CORS
+            var corsPolicyName = "AllowSpecificOrigins";
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy(corsPolicyName, policy =>
+                {
+                    policy.WithOrigins("http://localhost:3000", "http://localhost:5173") // Chỉ định các nguồn được phép truy cập
+                          .AllowAnyMethod()  // Cho phép tất cả các phương thức (GET, POST, PUT, DELETE,...)
+                          .AllowAnyHeader()  // Cho phép tất cả các headers
+                          .AllowCredentials(); // Cho phép gửi cookies và headers Authorization
+                });
+            });
 
             var app = builder.Build();
 
@@ -29,6 +43,7 @@ namespace Skincare.API
                 app.UseSwaggerUI();
             }
 
+            app.UseCors(corsPolicyName);
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
